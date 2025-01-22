d54aca4abfd39d215f28407ab5b3de4df176453c
Lasse Øland <oeland91@gmail.com>
2025-01-22 09:59:26 +0100

    Tilføjet mappe til sounds of lavet base class for animal

---
+++ Animals/animal.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+class Animal(ABC):
+    def __init__(self, name, year_of_birth):
+        self.name = name
+        self.year_of_birth = year_of_birth
+
+        def get_age():
+            age = datetime.now().year - self.year_of_birth
+            return f"Navn: {self.name}, alder: {age}"
+        
+        @abstractmethod
+        def make_sound(self):
+            pass
+            
